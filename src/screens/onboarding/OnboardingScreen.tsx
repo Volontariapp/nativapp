c8a11f5a0a0dc3713747b6b728ccd6d0ba41a99f
VictorAgahi <victor@agahi.com>
2026-05-25 22:06:38 +0200

    feat(ui): add logo to onboarding screen

--- src/screens/onboarding/OnboardingScreen.tsx
+++ src/screens/onboarding/OnboardingScreen.tsx
@@ -4,6 +4,8 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { AppButton } from '@/components/buttons/AppButton';
 import { AppText } from '@/components/typography/AppText';
 import { theme } from '@/shared/themes/theme';
+import { Image } from 'expo-image';
+import logo from '../../../assets/logo.jpg';
 import { useNavigation } from '@react-navigation/native';
 import type { AuthNavigationProp } from '@/navigation/stacks/AuthStack';
 
@@ -14,6 +16,7 @@ export function OnboardingScreen(): React.JSX.Element {
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
         <View style={styles.content}>
+          <Image source={logo} style={styles.logo} contentFit="contain" />
           <AppText style={styles.title}>Bienvenue sur Volontariapp</AppText>
           <AppText style={styles.subtitle}>
             Découvrez et rejoignez des missions de volontariat autour de vous.
@@ -56,6 +59,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: theme.spacing.xl,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    marginBottom: theme.spacing.xxl,
+    borderRadius: theme.radius.lg,
   },
   title: {
     fontSize: 28,
