e5104d91338fa6fd1da9f611cb8f8d1a462bbe54
victor.giroud <victor.giroud@epita.fr>
2026-06-16 11:35:17 +0200

    fix: eslint

--- src/api/user/hooks/use-get-public-user.ts
+++ src/api/user/hooks/use-get-public-user.ts
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { userApi, type UserProfile } from '../user.api';
+import { userApi, type UserPublicProfile } from '../user.api';
 
 const PROFILE_QUERY_KEY = ['profile'] as const;
 
@@ -8,7 +8,7 @@ const PROFILE_QUERY_KEY = ['profile'] as const;
  * Utilise React Query pour la gestion du cache, du loading et des erreurs.
  */
 export const useGetPublicUser = (userId: string) => {
-  return useQuery<UserProfile>({
+  return useQuery<UserPublicProfile>({
     queryKey: [...PROFILE_QUERY_KEY, userId],
     queryFn: async () => await userApi.getPublicUser(userId),
   });
